bf87cb5476e99361456879d88363d0389d70cc7b
Steve O'Gallagher <steve.ogallagher@cohaesus.co.uk>
2015-11-01 13:05:00 +0000

    Create bio object and add Name and Role to index.html using JS.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,24 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var bio = {
+	name: "Steve O'Gallagher",
+	role: "Developer",
+	contacts: {
+		mobile: "07725550475",
+		email: "ogallaghersteve@gmail.com",
+		github: "http://www.github.com/AddedSteve",
+		twitter: "http://www.twitter.com/AddedSteve",
+		location: "London, UK"
+	},
+	welcomeMessage: "Hello, and welcome to my resume.",
+	skills: ["HTML", "CSS", "JavaScript", "Python", "Swift"],
+	biopic: "https://pbs.twimg.com/profile_images/600016970780651520/MMUQ1gu7.jpg",
+	display: function () {
+		var formattedName = HTMLheaderName.replace("%data%", bio.name);
+		var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+		$('#header').prepend(formattedRole);
+		$('#header').prepend(formattedName);
+	}
+};
+
+bio.display();
+
+
